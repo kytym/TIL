04a56f016567510671f12c46daf560fda80d4dea
kytym <380kento@gmail.com>
2024-10-11 23:05:18 +0900

    add select sort

---
+++ pyAlgo/sort/select.py
@@ -0,0 +1,19 @@
+import random
+from typing import List
+
+
+def select_sort(numbers: List[int]) -> list[int]:
+    len_numbers = len(numbers)
+    for i in range(len_numbers):
+        min_idx = i
+        for j in range(i+1, len_numbers):
+            if numbers[min_idx] > numbers[j]:
+                min_idx = j
+        
+        numbers[i] , numbers[min_idx] = numbers[min_idx], numbers[i]
+    
+    return numbers
+
+if __name__ == "__main__":
+    numbers = [random.randint(0,100) for _ in range(10)]
+    print(select_sort(numbers))
